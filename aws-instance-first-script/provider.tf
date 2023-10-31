419318f1d72cc282e93136c00d2a1e0e9f430397
gowniraghu <48234482+gowniraghu@users.noreply.github.com>
2023-10-31 12:41:26 +0530

    provider update

--- aws-instance-first-script/provider.tf
+++ aws-instance-first-script/provider.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.23.1"
     }
   }
 }
-  region  = "${var.region}
+
+provider "aws" {
+  region = var.region
 }
